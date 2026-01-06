60e2b3a0e89e1c6bbd60110cdd5f09069cd56166
0130 <xsr0130@gmail.com>
2026-01-06 15:54:37 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("Could not find root element to mount to!");
 }
 
 const root = ReactDOM.createRoot(rootElement);
